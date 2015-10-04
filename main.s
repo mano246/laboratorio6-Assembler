19e9d58d7d0598dd0cbbbb8d7ce3f1958e6d5772
Manolo Ramírez Aguilar <mr.manololo@gmail.com>
2015-10-03 21:12:29 -0600

    Update main.s

--- main.s
+++ main.s
@@ -1,4 +1,3 @@
-
 .section .init
 .globl _start
 _start:
@@ -8,18 +7,15 @@ b main
 .section .text
 main:
 	mov sp,#0x8000
-
-
-	//Inicializacion de la pantalla con resolucion 1024x768.
+	
 	mov r0,#1024
 	mov r1,#768
 	mov r2,#16
 	bl InitialiseFrameBuffer
 
-	//Manejo del error al no poder reservar el frame buffer
 	teq r0,#0
 	bne noError$
-		
+	
 	mov r0,#16
 	mov r1,#1
 	bl SetGpioFunction
@@ -30,41 +26,42 @@ main:
 
 	error$:
 		b error$
-
-	//Pantalla Inicializada correctamente
+		
 	noError$:
-	
-	//fbInfoAddr es la direccion a la tabla con la informacion de la pantalla.
-	fbInfoAddr .req r1
-	mov fbInfoAddr,r0
-	
-	initDrawFondo: 
-		fbAddr .req r5
-		ldr fbAddr,[fbInfoAddr,#32]
+		mov r4,r0
 		
-		mov r6,#0			//countByte
-		
-		ldr r4, = fondo 
-		ldr r2, = fondoHeight
-		ldrh r2, [r2]
-		ldr r3, = fondoWidth
-		ldrh r3, [r3]
-		bl drawImage
-		
-		/*
-		*mov r6,#0
-		*ldr r4, = nemo
-		*ldr r2, = nemoHeight
-		*ldrh r2, [r2]
-		*ldr r3, = nemoWidth
-		*ldrh r3, [r3]
-		*bl drawImage
-		*/
-		b initDrawFondo
-		
+		//Empieza el ciclo infinito de filas y columnas
+		draw:
+			ldr r2, = fondoHeight
+			ldr r2, [r2]				//y
+			ldr r3, = fondoWidth
+			ldr r3, [r3]				//x
+			ldr r4, [r4, #32]			//fbAddr
+			ldr r5, =fondo
+			push {r5}
+			bl drawImage
 			
+		b draw
+			
+drawImage:
+	pop {r5}
+	push {lr}
+	mov r6, #0
+	mov r7, #0
+	inicioC:
+		inicioR:
+			ldr r5, = fondo
+			ldrh r6, [r5, r7]
+			strh r6, [r4]
+			add r4, #2
+			add r7, #2
+			sub r3, #1
+			
+			teq r3, #0
+			bne inicioR
+		sub r2, #1
+		teq r2, #0
+		bne inicioC
+	pop {pc}
 		
-
-			
-			
 		
